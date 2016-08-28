034d4d1f2883f34e2a4ebdbed8b27a223fd0b5f4
b1case <stefan@vt-berg.de>
2016-08-28 11:38:48 +0200

    Changing the Layout-Manager

--- pyArduSerialData/pyArduSerialData.py
+++ pyArduSerialData/pyArduSerialData.py
@@ -1,33 +1,72 @@
 from tkinter import *
 
-class App:
-	def __init__(self, master):
-		# master.minsize(width = 1200, height = 800)
-		frame = Frame(master)
-		frame.grid()
-		self.label = Label(frame, text = "Serielle Schnittstelle")
-		self.label.grid(row = 0, column = 0, columnspan = 2, padx = 20, pady = 10)
-		self.button = Button(frame, text = "Start", command = self.startdata)
-		self.button.grid(row = 1, column = 0, sticky = E, pady = 10)
-		self.button = Button(frame, text = "Stop", command = self.stopdata)
-		self.button.grid(row = 1, column = 1, sticky = W)
-		self.label = Label(frame, text = "Steuerung Aggregat")
-		self.label.grid(row = 0, column = 3, columnspan = 3, padx = 40)
-		self.button = Button(frame, text = "Kühlen", command = self.cool)
-		self.button.grid(row = 1, column = 3, sticky = E)
-		self.button = Button(frame, text = "Heizen", command = self.hot)
-		self.button.grid(row = 1, column = 4)
-		self.button = Button(frame, text = "Aus", command = self.aus)
-		self.button.grid(row = 1, column = 5, sticky = W)
-		self.label = Label(frame, text = "Steuerung Temperatur")
-		self.label.grid(row = 0, column = 6, columnspan = 2)
-		self.scale = Scale(frame, from_=10, to=42, orient = HORIZONTAL)
-		self.scale.grid(row = 1, column = 6)
-		self.button = Button(frame, text = "Senden", command = self.senden)
-		self.button.grid(row = 1, column = 7)
-		self.button = Button(frame, text = "Beenden", fg = "red", command = frame.quit)
-		self.button.grid(row = 1, column = 8, padx = 40, sticky = W)
+class App(Frame):
+	def __init__(self, master = None):
+			Frame.__init__(self, master)
+			self.grid()
+			self.master.title("Kühlaggregat")
+
+			FrameKopf = Frame(master, bg = "red")
+			FrameKopf.grid(row = 0, column = 0, rowspan = 2, columnspan = 10)
+        
+			FrameLinks = Frame(master, bg = "blue")
+			FrameLinks.grid(row = 2, column = 0, rowspan = 5, columnspan = 2)
+        
+			FrameRechts = Frame(master, bg = "green")
+			FrameRechts.grid(row = 2, column = 2, rowspan = 5, columnspan = 8 )
+        
+			FrameUnten = Frame(master, bg = "yellow")
+			FrameUnten.grid(row = 8, column = 0, columnspan = 10)
+        
+
+		
+			self.labelSer = Label(FrameKopf, text = "Serielle Schnittstelle")
+			self.buttonStart = Button(FrameKopf, text = "Start", fg = "green", height = 2, width = 6, command = self.startdata)
+			self.buttonStop = Button(FrameKopf, text = "Stop", fg = "red", height = 2, width = 6, command = self.stopdata)
+			self.labelAgg = Label(FrameKopf, text = "Steuerung Aggregat")
+			self.buttonKühlen = Button(FrameKopf, text = "Kühlen", height = 2, width = 6, command = self.cool)
+			self.buttonHeizen = Button(FrameKopf, text = "Heizen", height = 2, width = 6,command = self.hot)
+			self.buttonAus = Button(FrameKopf, text = "Aus", height = 2, width = 6, command = self.aus)
+			self.labelSTTemp = Label(FrameKopf, text = "Steuerung Temperatur")
+			self.scaleTemp = Scale(FrameKopf, from_=10, to=42, orient = HORIZONTAL)
+			self.buttonTemp = Button(FrameKopf, text = "Senden", height = 2, width = 6, command = self.senden)
+			self.buttonBeenden = Button(FrameKopf, text = "Beenden", fg = "red", height = 2, width = 6, command = master.quit)
+			self.labelAktuelleTemp = Label(FrameLinks, text = "Aktuelle Temperatur")
+			self.labelRaum = Label(FrameLinks, relief = SUNKEN, height = 2, width = 12, text = "Raum:")
+			self.textRaum = Text(FrameLinks, height = 2, width = 4)
+			self.labelTank = Label(FrameLinks, relief = SUNKEN, height = 2, width = 12, text = "Tank:")
+			self.textTank = Text(FrameLinks, height = 2, width = 4)
+			self.labelPelIn = Label(FrameLinks, relief = SUNKEN, height = 2, width = 12, text = "Pelitier Innen:")
+			self.textPelIn = Text(FrameLinks, height = 2, width = 4)
+			self.labelPelAu = Label(FrameLinks, relief = SUNKEN, height = 2, width = 12, text = "Pelitier Außen:")
+			self.textPelAu = Text(FrameLinks, height = 2, width = 4)
+			self.labelKonsole = Label(FrameUnten, text = "Konsole")
+			self.textKonsole = Text(FrameUnten, height = 2, width = 20)
 	
+			self.labelSer.grid(row = 0, column = 0, columnspan = 2)
+			self.labelAgg.grid(row = 0, column = 2, columnspan = 3)
+			self.labelSTTemp.grid(row = 0, column = 5, columnspan = 2)
+			self.buttonStart.grid(row = 1, column = 0)
+			self.buttonStop.grid(row = 1, column = 1)
+			self.buttonKühlen.grid(row = 1, column = 2)
+			self.buttonHeizen.grid(row = 1, column = 3)
+			self.buttonAus.grid(row = 1, column = 4)
+			self.scaleTemp.grid(row = 1, column = 5)
+			self.buttonTemp.grid(row = 1, column = 6)
+			self.buttonBeenden.grid(row = 1, column = 7)
+			self.labelAktuelleTemp.grid(row = 2, column = 0)
+			self.labelRaum.grid(row = 3, column = 0)
+			self.labelTank.grid(row = 4, column = 0)
+			self.labelPelIn.grid(row = 5, column = 0)
+			self.labelPelAu.grid(row = 6, column = 0)
+			self.textRaum.grid(row = 3, column = 1)
+			self.textTank.grid(row = 4, column = 1)
+			self.textPelIn.grid(row = 5, column = 1)
+			self.textPelAu.grid(row = 6, column = 1)
+			self.labelKonsole.grid(row = 8, column = 0)
+			self.textKonsole.grid(row = 8, column = 1)
+		
+
 	def startdata(self):
 		print ("Start")
 	def stopdata(self):
@@ -42,6 +81,5 @@ class App:
 		print(self.scale.get())
 
 root = Tk()
-root.wm_title("Kühlaggregat")
-app = App(root)
+app = App(master = root)
 root. mainloop()
